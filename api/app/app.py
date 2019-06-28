5eb3c9d8ae622886b159a0a84c98526dd82051e1
wildmar <marina.rhodes@gmail.com>
2019-06-27 19:48:54 -0700

    create initial flask app

---
+++ api/app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+from extensions import api
+
+DEFAULT_HOST = "0.0.0.0"
+
+
+def initialize_extensions(app):
+    """
+    Initializes the given Flask app with the extensions listed below:
+    Api - Flask RESTplus api
+
+    Args:
+        app: Constructed Flask app
+    """
+
+    api.init_app(app=app)
+
+
+def create_app():
+    """
+    Creates Flask app with initialized extensions
+    Returns:
+        Flask app with initialized extensions (Flask RESTplus, Flask SQLAlchemy, etc)
+    """
+    app = Flask(__name__)
+    initialize_extensions(app=app)
+    return app
+
+
+if __name__ == "__main__":
+    _app = create_app()
+    _app.run(host=DEFAULT_HOST, debug=True)
